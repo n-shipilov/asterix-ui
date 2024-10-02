74bd6a8fcfedf4f53566dd29974a6bd96ca44276
nshipilov <nshiplov@softailor.ru>
2024-10-02 15:57:18 +0300

    fix

--- src/components/theme/ThemeProvider.tsx
+++ src/components/theme/ThemeProvider.tsx
@@ -6,14 +6,34 @@ import { cn } from "../utils/cn";
 const block = cn("root");
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children, theme }) => {
-  document.body.classList.add(block());
+  const parentThemeState = React.useContext(ThemeContext);
+  const hasParentProvider = parentThemeState !== undefined;
+
+  console.log("hasParentProvider", hasParentProvider);
 
   React.useLayoutEffect(() => {
-    document.body.classList.toggle("st-root_theme_light", theme === "light");
-    document.body.classList.toggle("st-root_theme_dark", theme === "dark");
-  }, [theme]);
+    if (!hasParentProvider) {
+      document.body.classList.add(block());
+      document.body.classList.toggle("st-root_theme_light", theme === "light");
+      document.body.classList.toggle("st-root_theme_dark", theme === "dark");
+    }
+  }, [theme, hasParentProvider]);
 
   const contextValue = React.useMemo(() => ({ theme }) as ThemeContextProps, [theme]);
 
-  return <ThemeContext.Provider value={contextValue}>{children}</ThemeContext.Provider>;
+  return (
+    <ThemeContext.Provider value={contextValue}>
+      {hasParentProvider ? (
+        <div
+          className={block({
+            theme,
+          })}
+        >
+          {children}
+        </div>
+      ) : (
+        children
+      )}
+    </ThemeContext.Provider>
+  );
 };
