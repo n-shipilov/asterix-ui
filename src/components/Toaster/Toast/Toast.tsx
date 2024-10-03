6611e430d9edc7776bf3f7f47cdfcbd45cb43d33
nshipilov <nshiplov@softailor.ru>
2024-10-03 14:45:52 +0300

    color

--- src/components/Toaster/Toast/Toast.tsx
+++ src/components/Toaster/Toast/Toast.tsx
@@ -11,45 +11,35 @@ import { CloseCircleIcon } from "../../CloseCircleIcon";
 const block = cn("toast");
 
 const TITLE_ICONS: Record<ToastType, ReactNode> = {
-  info: (
-    <Icon data={InfoIcon} size={20} color="var(--st-color-brand-primary)" />
-  ),
-  success: (
-    <Icon data={InfoIcon} size={20} color="var(--st-color-base-success)" />
-  ),
-  warning: (
-    <Icon data={InfoIcon} size={20} color="var(--st-color-base-warning)" />
-  ),
-  error: (
-    <Icon data={CloseCircleIcon} size={20} color="var(--st-color-base-error)" />
-  ),
+  info: <Icon data={InfoIcon} size={20} color="var(--st-color-brand-primary)" />,
+  success: <Icon data={InfoIcon} size={20} color="var(--st-color-base-success)" />,
+  warning: <Icon data={InfoIcon} size={20} color="var(--st-color-base-warning)" />,
+  error: <Icon data={CloseCircleIcon} size={20} color="var(--st-color-base-danger)" />,
 };
 
-export const Toast = forwardRef<HTMLDivElement, InternalToastProps>(
-  (props, ref) => {
-    const { content, title, type, id } = props;
+export const Toast = forwardRef<HTMLDivElement, InternalToastProps>((props, ref) => {
+  const { content, title, type, id } = props;
 
-    const { remove } = useToasterContext();
+  const { remove } = useToasterContext();
 
-    const onClose = () => remove(id);
+  const onClose = () => remove(id);
 
-    const renderIconByType = (type?: ToastType) => {
-      if (!type) {
-        return null;
-      }
+  const renderIconByType = (type?: ToastType) => {
+    if (!type) {
+      return null;
+    }
 
-      return TITLE_ICONS[type];
-    };
+    return TITLE_ICONS[type];
+  };
 
-    return (
-      <div className={block({ type })} ref={ref}>
-        <div className={block("icon")}>{renderIconByType(type)}</div>
-        {title && <div className={block("title")}>{title}</div>}
-        {content && <div className={block("content")}>{content}</div>}
-        <button className={block("btn-close")} onClick={onClose}>
-          <Icon data={CloseIcon} size={16} />
-        </button>
-      </div>
-    );
-  }
-);
+  return (
+    <div className={block({ type })} ref={ref}>
+      <div className={block("icon")}>{renderIconByType(type)}</div>
+      {title && <div className={block("title")}>{title}</div>}
+      {content && <div className={block("content")}>{content}</div>}
+      <button className={block("btn-close")} onClick={onClose}>
+        <Icon data={CloseIcon} size={16} />
+      </button>
+    </div>
+  );
+});
